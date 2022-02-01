44f016cb773b895f30271055de49a20d13a42b75
John Aragon <johnia0722@gmail.com>
2022-01-31 20:53:04 -0500

    hw 3 update

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -12,15 +12,28 @@ I certify that this assignment is my own work, but I discussed it with: <Name(s)
 
 
 def average():
-    pass
+    limit = eval(input("How many grades will you enter? "))
+    total = 0
+    for i in range(0,limit):
+        grade = eval(input("Enter Grade: "))
+        total = total + grade
+    print("Average: ", (total / limit))
 
 
 def tip_jar():
-    pass
+    total_tips = 0
+    for i in range(0, 5):
+        total_tips = total_tips + eval(input("How much will you tip? "))
+    print("The total amount of tips is ", total_tips)
 
 
 def newton():
-    pass
+    base = eval(input("What number would you like to find the square root of? "))
+    init = base
+    improve = eval(input("How many times should we improve the approximation? "))
+    for i in range(0, improve):
+        base = (base + (init / base)) / 2
+    print(base)
 
 
 def sequence():
