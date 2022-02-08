08389e5e366c61b7f3f9d4963814bf8b8f5eac3a
John Aragon <johnia0722@gmail.com>
2022-02-08 14:42:06 -0500

    Hmwrk 4

---
+++ assignments/hw4/hw4.py
@@ -0,0 +1,24 @@
+"""
+Name: John Aragon
+<ProgramName>.py
+
+Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+
+Certification of Authenticity:
+<include one of the following>
+I certify that this assignment is entirely my own work.
+I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+"""
+
+from graphics import GraphWin, Circle, Point, Polygon
+from math import sqrt
+
+def squares(x, y, G):
+    square = Polygon()
+
+def prob_1():
+    win = GraphWin("title", 700, 700)
+
+    win.getMouse()
+
+prob_1()
