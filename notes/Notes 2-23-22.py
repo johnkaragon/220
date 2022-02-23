4c0644e6c5cf6064d0143261e6fb4c4ce93d5ac2
John Aragon <johnia0722@gmail.com>
2022-02-23 11:20:33 -0500

    notes

---
+++ notes/Notes 2-23-22.py
@@ -0,0 +1,31 @@
+from graphics import Point
+from math import *
+
+
+def distance(p1, p2):
+    x1 = p1.getX()
+    y1 = p1.getY()
+    x2 = p2.getX()
+    y2 = p2.getY()
+    square = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2))
+    return square
+
+
+def sum_diff(x, y):
+    sum_val = x + y
+    diff_val = x - y
+    return sum_val, diff_val
+
+
+def get_discount(price, sale):
+    price = price - (price * sale)
+    return price
+
+
+def change_point(p, x, y):
+    p.move(x, y)
+
+
+print(sum_diff(3, 10))
+print(distance(Point(3, 40), Point(30, 7)))
+print(get_discount(23.40, .15))
