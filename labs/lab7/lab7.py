688696b69d1f9785f498435353b76acf54f582bd
johnkaragon <johnia0722@gmail.com>
2022-03-14 19:52:54 -0400

    lab 7

--- labs/lab7/lab7.py
+++ labs/lab7/lab7.py
@@ -2,7 +2,6 @@ def avg_calc(line):
     line_1 = line.lstrip(" ").split(" ")
     total = 0
     t_weight = 0
-    print(line_1)
     for i in range(0, len(line_1), 2):
         t_weight += eval(line_1[i])
         if t_weight > 100:
@@ -12,18 +11,28 @@ def avg_calc(line):
         return "weight is less than 100"
     total = total / 100
     form = "{decimal:.1f}"
-    return form.format(decimal=total)
+    return eval(form.format(decimal=total))
 
 
-def process(input_file, output_file):
+def weighted_average(input_file, output_file):
     reading = open(input_file, "r")
     text = reading.read().split("\n")
     new_text = ""
+    class_total = 0
+    class_counter = 0
     for i in range(0, len(text)):
-        line = text[i].split(":")
-        new_line = line[0] + "'s average: " + avg_calc(line[1]) + "\n"
+        read_line = text[i].split(":")
+        avg = avg_calc(read_line[1])
+        if type(avg) == float:
+            class_total += avg
+            class_counter += 1
+        new_line = read_line[0] + "'s average: " + str(avg) + "\n"
         new_text += new_line
     reading.close()
+    class_total = class_total / class_counter
+    form = "{decimal:.1f}"
+    nclass_total = form.format(decimal=class_total)
+    new_text += "class average: " + str(nclass_total)
     writing = open(output_file, "w")
     print(new_text, file=writing)
     writing.close()
@@ -31,7 +40,7 @@ def process(input_file, output_file):
 
 def main():
     if __name__ == "__main__":
-        process("grades.txt", "avg.txt")
+        weighted_average("grades.txt", "avg.txt")
 
 
 main()
