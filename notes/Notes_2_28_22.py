58cb871d2f9591707493842103a7c18e270f9fdf
John Aragon <johnia0722@gmail.com>
2022-03-14 10:31:00 -0400

    hw 7

--- notes/Notes_2_28_22.py
+++ notes/Notes_2_28_22.py
@@ -1,6 +1,24 @@
 import math
 
 
+def quadratic_1(a, b, c):
+    try:
+        disc = b * b - 4 * a * c
+        sqrt_discr = math.sqrt(disc)
+        denom = 2 * a
+        root_1 = (-b + sqrt_discr) / denom
+        root_2 = (-b - sqrt_discr) / denom
+        print(root_1, ", ", root_2)
+    except ValueError as e:
+        if str(e) == "math domain error":
+            print("no real roots")
+    except SyntaxError:
+        print("invalid input")
+
+
+quadratic_1(1, 1, 3)
+
+
 def quadratic(a, b, c):
     disc = b * b - 4 * a * c
     if disc < 0:
@@ -8,12 +26,12 @@ def quadratic(a, b, c):
     # if disc >= 0:
     else:
         if disc == 0:
-            sqrt_discr = math.sqrt()
+            sqrt_discr = math.sqrt(disc)
             denom = 2 * a
             root_1 = (-b + sqrt_discr) / denom
             print(root_1)
         else:
-            sqrt_discr = math.sqrt()
+            sqrt_discr = math.sqrt(disc)
             denom = 2 * a
             root_1 = (-b + sqrt_discr) / denom
             root_2 = (-b - sqrt_discr) / denom
---
+++ notes/Notes_3_4_22.py
@@ -0,0 +1,21 @@
+"""
+
+for loops: definite loop
+    - Knows where it'll start and end
+
+indefinite loops:
+    - Knows where they'll star, but not when they'll stop
+
+while <condition> :
+    <body>
+
+
+"""
+
+for i in range(5):
+    print(i)
+
+counter = 0
+while counter != 5:
+    print(counter)
+    counter += 1
