11fac74d617852d341a7b6f885f899a3dad3b58a
John Aragon <johnia0722@gmail.com>
2022-03-14 11:17:51 -0400

    notes

--- notes/Notes_3_4_22.py
+++ notes/Notes_3_4_22.py
@@ -11,11 +11,33 @@ while <condition> :
 
 
 """
+from graphics import *
 
-for i in range(5):
-    print(i)
 
-counter = 0
-while counter != 5:
-    print(counter)
-    counter += 1
+def my_first_while():
+    for i in range(5):
+        print(i)
+
+    counter = 0
+    while counter != 5:
+        print(counter)
+        counter += 1
+
+
+def is_equal(p1, p2):
+    return p1.getX() == p2.getX() and p1.getY() == p2.getY()
+
+
+def is_game_over(p1_points, p2_points):
+    if p1_points >= 15 or p2_points >= 15:
+        return True
+    return False
+
+
+player1 = 9
+player2 = 15
+while not is_game_over(player1, player2):
+    one, two = eval(input("enter xcore for player one and two"))
+    player1 += one
+    player2 += two
+
